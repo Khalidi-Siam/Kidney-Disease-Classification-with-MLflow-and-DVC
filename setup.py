93c76ffffd84c9887c058e2a9d7ac4bfd53a342c
Khalidi-Siam <siam074@yahoo.com>
2026-05-10 22:10:28 +0600

    logger, exception added

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function reads the requirements from a file and returns them as a list.
+    '''
+    with open(file_path, 'r') as file:
+        requirements = file.read().splitlines()
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+__version__ = '0.0.1'
+REPO_NAME = "Kidney-Disease-Classification"
+AUTHOR_USER_NAME = "khalidi-siam"
+SRC_REPO = "Kidney-disease-classification with MLflow and DVC"
+AUTHOR_EMAIL = "siam074@yahoo.com"
+
+
+setup(
+    name='Kidney-disease-classification',
+    version='0.0.1',
+    author="Khalidi Siam",
+    author_email="siam074@yahoo.com",
+    description="End to End DL Project with MLflow and DVC",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=get_requirements('requirements.txt'),
+)
--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "chicken_disease_classification"
+project_name = "kidney_disease_classification"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
