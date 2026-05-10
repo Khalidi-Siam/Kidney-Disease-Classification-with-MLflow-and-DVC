3abb42f4b0756d19b214225862296b0aabc36ccb
Khalidi-Siam <siam074@yahoo.com>
2026-05-10 23:51:41 +0600

    prepare base model completed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kidney_disease_classification.logger import logging
 from kidney_disease_classification.exception import CustomException
 from kidney_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from kidney_disease_classification.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from kidney_disease_classification.pipeline.stage_03_prepare_base_model import PrepareBaseModelTrainingPipeline
+from kidney_disease_classification.pipeline.stage_03_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from kidney_disease_classification.pipeline.stage_04_training import ModelTrainingPipeline
 # from kidney_disease_classification.pipeline.stage_05_evaluation import EvaluationPipeline
 
@@ -24,6 +24,16 @@ try:
     logging.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logging.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    raise CustomException(e, sys)
+
+
+STAGE_NAME = "Prepare Base Model stage"
+try:
+    logging.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logging.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
 except Exception as e:
     raise CustomException(e, sys)
--- src/kidney_disease_classification/entity/config_entity.py
+++ src/kidney_disease_classification/entity/config_entity.py
@@ -15,3 +15,15 @@ class DataValitationConfig:
     root_dir: Path
     unzip_data_dir: Path
     STATUS_FILE: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
