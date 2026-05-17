e9884cd638b6908af2632bc5d05b0b36485a1bcf
khalidi-siam <siam074@yahoo.com>
2026-05-18 01:08:04 +0600

    dvc added

--- src/kidney_disease_classification/exception.py
+++ src/kidney_disease_classification/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.kidney_disease_classification.logger import logging
+from kidney_disease_classification.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
